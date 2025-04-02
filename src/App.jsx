efe832dce16a0a4ae344452e1ec6465a9ddb9289
shelendra <sheltech@shelendras-MacBook-Air.local>
2025-04-02 11:09:33 +0530

    name add

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello My Name is Shelendra Singh ARPITA</h1>
+        <h1>Hello My Name is Shelendra Singh kapil singh</h1>
       </div>
     </>
   )
